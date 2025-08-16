4b948428ae2cc7a7953e9f7a2cffc700f024a4bc
Yograj2024 <ryog7910@gmail.com>
2025-08-16 11:38:08 -0400

    designed gpt search page UI and implemented conditional css styling on hereSection

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -15,11 +15,11 @@ const Body = () => {
   useTopRated();
 
   const headerBeforeCss = " before:content-[''] before:h-[37.95rem] before:w-full before:absolute before:inset-0 before:z-[-1] before:bg-cover before:bg-center before:bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/258d0f77-2241-4282-b613-8354a7675d1a/web/IN-en-20250721-TRIFECTA-perspective_cadc8408-df6e-4313-a05d-daa9dcac139f_medium.jpg')]"
-  const headerAfterCss = "after:content-[''] after:w-full after:absolute after:top-0 after:h-[37.9rem] after:bg-black/40 after:z-[-1]"
+  const headerAfterCss = "after:content-[''] after:w-full after:absolute after:top-0 after:h-[37.9rem] after:bg-black/60 after:z-[-1]"
 
   return moviesList === null ? <ShimmerUI/> : <>
     <header className ={`relative ${headerAfterCss}
-      after:h-[${!user ? "37.95rem" : gpt ? "37.95rem" : "47.9rem"}]
+      ${!user ? "after:h-[37.95rem]" : gpt ? "after:h-[37.95rem]" : "after:h-[47.9rem]"}
        ${user ? (gpt ? "" : "h-[47.9rem]") : ""}
       ${(!user || (user && gpt)) && `${headerBeforeCss}`} 
     `}> {/* explain in line no. 39 */}
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,29 +1,45 @@
 import { useSelector } from "react-redux";
 import MoviesRow from "./MoviesRow";
+import { useState } from "react";
 
 const Main = () => {
 
-  const trending   =  useSelector( store => store.movies.nowPlayingMovies)
-  const topRated   =  useSelector( store => store.movies.topRatedMovie)
-  const gpt        =  useSelector( store => store.gptSearchPage.isShowGPTSearchPage);
-  const inputBoxCSS = `placeholder:capitalize placeholder:text-gray-200 outline-none h-[2.8rem] w-[50%] border-[1px] border-[#605F5E] rounded-[0.45rem] px-[1.5rem] bg-black/40`
+  const trending    =  useSelector( store => store.movies.nowPlayingMovies)
+  const topRated    =  useSelector( store => store.movies.topRatedMovie)
+  const gpt         =  useSelector( store => store.gptSearchPage.isShowGPTSearchPage);
+  const [userInput, setUserInput] = useState("");
+  const inputBoxCSS = `relative placeholder:capitalize placeholder:text-gray-200 outline-none h-[4rem] 
+  w-full border-[1px] border-[#605F5E] focus:border-red-600 rounded-[0.3rem] px-[1.5rem] bg-black/40 pt-[1rem] focus:shadow-[0px_0px_5px__5px_rgba(255,0,0,0.3)]`
 
 
   return !gpt ? 
-  <div className ={`pb-[3rem] pl-[2rem]`}>
-    <MoviesRow title={"trending now"} movieList={trending} />
-    <MoviesRow title={"top rated"} movieList={topRated} />
-  </div> : 
-  
-  <div className={`w-[50%] m-auto pt-[5rem]`}>
-      <h2 className={`text-white text-[2.5rem] font-semibold capitalize leading-[3.5rem] text-center px-[2rem]`}>describe what you want to watch, we’ll suggest...</h2>
-      <p className={`text-white text-[1.125rem] font-semibold px-[3rem] capitalize mt-[1rem]`}>skip the endless scrolling and start watching something you’ll love.</p>
+    <div className ={`pb-[3rem] pl-[2rem]`}>
+      <MoviesRow title={"trending now"} movieList={trending} />
+      <MoviesRow title={"top rated"} movieList={topRated} />
+    </div> : 
+    
+    <div className={`pt-[5rem] text-white capitalize`}>
+        <h2 className={`text-center text-[3.25rem] font-bold leading-[4rem] px-[21rem]`}>describe what you want to watch, we’ll suggest...</h2>
+        <p className={`text-center text-[1.25rem] font-bold mt-[1rem]`}>let us know your mood</p>
+        <h3 className={`text-center text-[1.125rem] font-semibold mt-[2rem]`}>skip the endless scrolling, find and watching something you’ll love</h3>
 
-      <div className={`mt-[3rem] pl-[5rem] flex flex-col text-white`}>
-        <label htmlFor="userDescription" className={`ml-[0.5rem] mb-[0.8rem]`}>your description</label>
-        <input type="text" name="fullName" id="" className ={`${inputBoxCSS}`}/>
-      </div>
-  </div>
+        <div className={`flex items-center justify-center gap-x-[1rem] mt-[2rem]`}>
+          <div className={`relative flex-[0_0_32%] group`}>
+            <label htmlFor="userDiscription" 
+            className ={`${ userInput ? "top-[8%] text-gray-400" : "group-focus-within:top-[8%] group-focus-within:text-[0.9rem] group-focus-within:text-gray-400 top-[26%] text-[1.25rem]"} absolute z-[1] font-semibold tracking-[1px] left-[6%] transition-all duration-200 `}>
+              discription
+            </label>
+            <input type="text" id="userDiscription" name="fullName" 
+            className = {` ${ inputBoxCSS } `}
+            value={userInput}
+            onChange={ (e) => setUserInput( e.target.value)}/>
+          </div>
+          <button className={`bg-red-600 flex items-center px-[2rem] py-[0.9rem] rounded-[0.6rem] text-[1.5rem] capitalize font-semibold`}>
+            get start
+            <span className={`inline-block text-[1rem] ml-[0.8rem] pt-[0.2rem]`}>&#x276F;</span>
+          </button>
+        </div>
+    </div>
 }; 
 
 export default Main; 
